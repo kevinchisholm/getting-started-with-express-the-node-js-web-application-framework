da728af84a51164d15b226324c322e889cb18cdd
Kevin Chisholm <kevinchisholm@Kevins-MBP.home>
2016-07-18 20:41:01 -0400

    add package.json and example-1.js

---
+++ example-1.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	var HTML = ''
+	+ '<!DOCTYPE html>'
+	+ '<html>'
+		+ '<head>'
+			+ '<meta charset=utf-8>'
+			+ '<title>My Web Page</title>'
+		+ '</head>'
+		+ '<body>'
+			+ '<h1>It works!</h1>'
+		+ '</body>'
+	+ '</html>';
+
+  res.send(HTML);
+});
+
+app.listen(3000, function () {
+  console.log('Server is running. Point your browser to: http://localhost:3000');
+});
